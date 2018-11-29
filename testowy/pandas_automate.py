d2f5796a14ab3ea397ac86d3d12f336bec44183f
JACH SLAWOMIR <jach.slawomir.external@atos.net>
2018-11-29 15:16:36 +0100

    don't know what is this

--- testowy/pandas_automate.py
+++ testowy/pandas_automate.py
@@ -13,22 +13,22 @@ def sdk_ver():
 sdk_ver()
 
 
+def ndk_ver():
 
-calls_ndk = pd.read_html("https://developer.android.com/ndk/downloads/" , header=0, parse_dates=["Platform"])
-var2 = calls_ndk[0]
-
-
-json_file_sdk = var1.to_json()
-json_file_ndk = var2.to_json()
-
-data_sdk = json.loads(json_file_sdk)
-print(data_sdk["SDK tools package"]["2"])
-data_ndk = json.loads(json_file_ndk)
-print(data_ndk["Package"]["3"])
+    calls_ndk = pd.read_html("https://developer.android.com/ndk/downloads/" , header=0, parse_dates=["Platform"])
+    var1 = calls_ndk[0]
+    json_file_ndk = var1.to_json()
+    data_ndk = json.loads(json_file_ndk)
+    print(data_ndk["Package"]["3"])
 
 
-#print(json_file_sdk[4])
-#print(json_file_ndk)
+ndk_ver()
+
+
+
+
+
+
 
 
 #ant = pd.read_html("https://ant.apache.org/antnews.html")
