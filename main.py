6770c3cd033bf568b1deb85aecc1ef1e7082a3a8
JACH SLAWOMIR <jach.slawomir.external@atos.net>
2018-12-03 09:08:38 +0100

    first working stage

--- main.py
+++ main.py
@@ -1,9 +1,88 @@
 
-
+import json
 import requests
 import justext
+import sys
+from request_info import Request
 
-response = requests.get("")
-paragraphs = justext.justext(response.content, justext.get_stoplist("English"))
-for paragraph in paragraphs:
-    print(paragraph.text)
+
+def campare_gradle():
+
+    with open("info_files/current_version.json", "r") as read_file:
+        data = json.load(read_file)
+        if data["Aps"]["Gradle"]["version"] == Request.gradle_ver():
+            print("Gradle is in the same version as in project")
+        else:
+            print("New Gradle version !!!")
+
+
+campare_gradle()
+
+
+def campare_maven():
+
+    with open("info_files/current_version.json", "r") as read_file:
+        data = json.load(read_file)
+        if data["Aps"]["Maven"]["version"] == Request.maven_ver():
+            print("Maven is in the same version as in project")
+        else:
+            print("New Maven version !!!")
+
+
+campare_maven()
+
+
+def campare_ndk():
+
+    with open("info_files/current_version.json", "r") as read_file:
+        data = json.load(read_file)
+        if data["Aps"]["Android NDK"]["version"] == Request.ndk_ver():
+            print("NDK is in the same version as in project")
+        else:
+            print("New NDK version !!!")
+
+
+campare_ndk()
+
+
+def campare_sdk():
+
+    with open("info_files/current_version.json", "r") as read_file:
+        data = json.load(read_file)
+        if data["Aps"]["Android SDK"]["version"] == Request.sdk_ver():
+            print("SDK is in the same version as in project")
+        else:
+            print("New SDK version !!!")
+
+
+campare_sdk()
+
+
+def campare_infer():
+
+    with open("info_files/current_version.json", "r") as read_file:
+        data = json.load(read_file)
+        if data["Aps"]["Infer"]["version"] == Request.infer_ver():
+            print("Infer is in the same version as in project")
+        else:
+            print("New Infer version !!!")
+
+
+campare_infer()
+
+
+def campare_ant():
+
+    with open("info_files/current_version.json", "r") as read_file:
+        data = json.load(read_file)
+        if data["Aps"]["Ant"]["version"] == Request.ant_ver():
+            print("Ant is in the same version as in project")
+        else:
+            print("New Ant version !!!")
+
+
+campare_ant()
+
+
+
+
--- request_info.py
+++ request_info.py
@@ -3,71 +3,45 @@ import pandas as pd
 import json
 
 
-def ant_ver():
-    response = requests.get("https://api.github.com/repos/apache/ant/tags")
-    todos = json.loads(response.text)
+class Request(object):
 
-    print(todos[1]["name"])
-    return todos[1]["name"]
-
-
-ant_ver()
+    @staticmethod
+    def ant_ver():
+        response = requests.get("https://api.github.com/repos/apache/ant/tags")
+        todos = json.loads(response.text)
+        return todos[1]["name"]
 
 
-def gradle_ver():
-    response = requests.get("https://services.gradle.org/versions/current")
-    todos = json.loads(response.text)
+    @staticmethod
+    def gradle_ver():
+        response = requests.get("https://services.gradle.org/versions/current")
+        todos = json.loads(response.text)
+        return todos['version']
 
-    print(todos['version'])
-    return todos['version']
+    @staticmethod
+    def maven_ver():
+        response = requests.get("https://api.github.com/repos/apache/maven/tags")
+        todos = json.loads(response.text)
+        return todos[1]["name"]
 
+    @staticmethod
+    def infer_ver():
+        response = requests.get("https://api.github.com/repos/facebook/infer/releases/latest")
+        todos = json.loads(response.text)
+        return todos["tag_name"]
 
-gradle_ver()
+    @staticmethod
+    def sdk_ver():
+        calls_df = pd.read_html("https://developer.android.com/studio/", header=0, parse_dates=["Platform"])
+        var1 = calls_df[1]
+        json_file_sdk = var1.to_json()
+        data_sdk = json.loads(json_file_sdk)
+        return data_sdk["SDK tools package"]["2"]
 
-
-def maven_ver():
-    response = requests.get("https://api.github.com/repos/apache/maven/tags")
-    todos = json.loads(response.text)
-
-    print(todos[1]["name"])
-    return todos[1]["name"]
-
-
-maven_ver()
-
-
-def infer_ver():
-    response = requests.get("https://api.github.com/repos/facebook/infer/releases/latest")
-    todos = json.loads(response.text)
-
-    print(todos["tag_name"])
-    return todos["tag_name"]
-
-
-infer_ver()
-
-
-"""This is Pandas librares section """
-
-
-def sdk_ver():
-    calls_df = pd.read_html("https://developer.android.com/studio/", header=0, parse_dates=["Platform"])
-    var1 = calls_df[1]
-    json_file_sdk = var1.to_json()
-    data_sdk = json.loads(json_file_sdk)
-    print(data_sdk["SDK tools package"]["2"])
-
-
-sdk_ver()
-
-
-def ndk_ver():
-
-    calls_ndk = pd.read_html("https://developer.android.com/ndk/downloads/", header=0, parse_dates=["Platform"])
-    var1 = calls_ndk[0]
-    json_file_ndk = var1.to_json()
-    data_ndk = json.loads(json_file_ndk)
-    print(data_ndk["Package"]["3"])
-
-
-ndk_ver()
+    @staticmethod
+    def ndk_ver():
+        calls_ndk = pd.read_html("https://developer.android.com/ndk/downloads/", header=0, parse_dates=["Platform"])
+        var1 = calls_ndk[0]
+        json_file_ndk = var1.to_json()
+        data_ndk = json.loads(json_file_ndk)
+        return data_ndk["Package"]["3"]
